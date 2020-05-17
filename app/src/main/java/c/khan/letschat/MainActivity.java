4d08ecf7034b099b45f3c4f8b331ce5833a1b9c4
Muhammad Sakib Khan <sakib2131@gmail.com>
2020-05-17 23:35:29 +0600

    Firebase API integrated and username request added

--- app/src/main/java/c/khan/letschat/MainActivity.java
+++ app/src/main/java/c/khan/letschat/MainActivity.java
@@ -1,13 +1,62 @@
 package c.khan.letschat;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView LiveChats;
+    ArrayList<String> ChatList = new ArrayList<String>();
+    ArrayAdapter ChatAdpt;
+
+    String UserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LiveChats = (ListView) findViewById(R.id.LiveChats);
+        ChatAdpt = new ArrayAdapter(this, android.R.layout.simple_list_item_1, ChatList);
+        LiveChats.setAdapter(ChatAdpt);
+
+        getUserName();
+
     }
+
+    private void getUserName(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final EditText userName = new EditText(this);
+
+        builder.setView(userName);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                UserName = userName.getText().toString();
+
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                getUserName();
+
+            }
+        });
+
+        builder.show();
+    }
+
+
 }
+
+
+
