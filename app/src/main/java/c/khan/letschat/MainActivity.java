469f6dbeb31136e0d7f5efeb9848a07c375f110f
Muhammad Sakib Khan <sakib2131@gmail.com>
2020-05-18 00:16:08 +0600

    Communication with Firebase done and Firebase database set with app

--- app/src/main/java/c/khan/letschat/MainActivity.java
+++ app/src/main/java/c/khan/letschat/MainActivity.java
@@ -1,14 +1,28 @@
 package c.khan.letschat;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter ChatAdpt;
 
     String UserName;
+
+    private DatabaseReference FirebaseDB = FirebaseDatabase.getInstance().getReference().getRoot();
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +47,40 @@ public class MainActivity extends AppCompatActivity {
         LiveChats.setAdapter(ChatAdpt);
 
         getUserName();
+
+        FirebaseDB.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Set<String> set = new HashSet<String>();
+                Iterator i = dataSnapshot.getChildren().iterator();
+
+                while (i.hasNext()){
+                    set.add(((DataSnapshot)i.next()).getKey());
+                }
+
+                ChatAdpt.clear();
+                ChatAdpt.addAll(set);
+                ChatAdpt.notifyDataSetChanged();
+            }
+
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        LiveChats.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent j = new Intent(getApplicationContext(), ChatActivity.class);
+
+                j.putExtra("selected_topic", ((TextView)view).getText().toString());
+                j.putExtra("user_name",UserName);
+                startActivity(j);
+            }
+        });
 
     }
 
